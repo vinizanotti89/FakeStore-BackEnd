f6b3e31b2d7ec65d1adb47e07ba4023119e72b87
Vinícius Zanotti de Oliveira <vinizanotti@gmail.com>
2025-09-23 01:18:59 -0300

    implement access mongoDB external

--- src/database/index.js
+++ src/database/index.js
@@ -10,23 +10,36 @@ import Category from "../app/models/Category.js";
 const models = [User, Product, Category];
 
 class Database {
-    constructor() {
-        this.init();
-        this.mongo();
-    }
+  constructor() {
+    this.initRelational();
+    this.initMongo();
+  }
 
-    init() {
-        this.connection = new Sequelize(configDatabase);
-        models
-            .map((model) => model.init(this.connection))
-            .map((model) => model.associate && model.associate(this.connection.models)
-            );
-    }
+  // Conexão Sequelize (Postgres)
+  initRelational() {
+    this.connection = new Sequelize(configDatabase);
 
-    mongo(){
-        this.connection = mongoose.connect('mongodb://localhost:27017/fakestore',
-        );
+    models
+      .map((model) => model.init(this.connection))
+      .map(
+        (model) => model.associate && model.associate(this.connection.models)
+      );
+  }
+
+  // Conexão Mongoose (MongoDB)
+  async initMongo() {
+    const mongoUri =
+      process.env.MONGO_URI || "mongodb://localhost:27017/fakestore";
+
+    try {
+      this.mongoConnection = await mongoose.connect(mongoUri);
+      console.log(
+        `[MongoDB] Conectado em: ${process.env.MONGO_URI ? "Atlas" : "Local"}`
+      );
+    } catch (err) {
+      console.error("[MongoDB] Erro ao conectar:", err.message);
     }
+  }
 }
 
 export default new Database();
